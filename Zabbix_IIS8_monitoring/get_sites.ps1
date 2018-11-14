20c6e797764245be68103385a504494d6b235264
Marco Hofmann <xenadmin@users.noreply.github.com>
2018-11-14 17:50:13 +0100

    Update get_sites.ps1
    A more modern approach which much less code and fast execution.

--- Zabbix_IIS8_monitoring/get_sites.ps1
+++ Zabbix_IIS8_monitoring/get_sites.ps1
@@ -1,23 +1,3 @@
-Import-Module WebAdministration
-$sites = Get-ChildItem -Path IIS:\Sites -Name
-$idx = 1
-write-host "{"
-write-host " `"data`":[`n"
-foreach ($currentsite in $sites)
-{
-    if ($idx -lt $sites.count)
-    {
-     
-        $line= "{ `"{#SITE}`" : `"" + $currentsite + "`" },"
-        write-host $line
-    }
-    elseif ($idx -ge $site.count)
-    {
-    $line= "{ `"{#SITE}`" : `"" + $currentsite + "`" }"
-    write-host $line
-    }
-    $idx++;
-}
-write-host
-write-host " ]"
-write-host "}"
+$sites = Get-IISSite | select-object -property @{N='{#SITE}';E={$_.Name}}
+$llddata = @{"data" = $sites}
+$llddata | ConvertTo-Json -Compress
