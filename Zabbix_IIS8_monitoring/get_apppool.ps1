c9268ead8d57f863cf930dbc61b3dbfc707d027c
Marco Hofmann <xenadmin@users.noreply.github.com>
2018-11-14 17:48:12 +0100

    Update get_apppool.ps1
    A more modern approach which much less code and fast execution.

--- Zabbix_IIS8_monitoring/get_apppool.ps1
+++ Zabbix_IIS8_monitoring/get_apppool.ps1
@@ -1,23 +1,3 @@
-Import-Module WebAdministration
-$apppool = Get-ChildItem -Path IIS:\Apppools -Name
-$idx = 1
-write-host "{"
-write-host " `"data`":[`n"
-foreach ($currentapppool in $apppool)
-{
-    if ($idx -lt $apppool.count)
-    {
-     
-        $line= "{ `"{#APPPOOL}`" : `"" + $currentapppool + "`" },"
-        write-host $line
-    }
-    elseif ($idx -ge $apppool.count)
-    {
-    $line= "{ `"{#APPPOOL}`" : `"" + $currentapppool + "`" }"
-    write-host $line
-    }
-    $idx++;
-}
-write-host
-write-host " ]"
-write-host "}"
+$rawdata = Get-IISAppPool | select-object -property @{N='{#APPPOOL}';E={$_.Name}}
+$jsondata = @{"data" = $rawdata}
+$jsondata | ConvertTo-Json -Compress
