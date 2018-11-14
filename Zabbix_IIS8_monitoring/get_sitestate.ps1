f19e310b0b6d10cb6353f316ceed332afa5a0d50
Marco Hofmann <xenadmin@users.noreply.github.com>
2018-11-14 18:33:43 +0100

    Update get_sitestate.ps1
    A complete different approach, where the result is expressed as a numeric value, which fits much better to the way Zabbix "thinks & works" internally. Could be expanded by a value mapping inside Zabbix Frontend. 
    A trigger would assume, that 'last' should be <> 1

--- Zabbix_IIS8_monitoring/get_sitestate.ps1
+++ Zabbix_IIS8_monitoring/get_sitestate.ps1
@@ -1,4 +1,17 @@
-param ([string] $name = 0)
-Import-Module WebAdministration
+param (
+    [string] $name = 0
+)
 $siteState = Get-WebsiteState -name "$name"
-Write-Output ($siteState.value)
+
+$ResultText = $siteState.Value
+$ResultNumber = 99
+#Valid values are Starting, Started, Stopping, Stopped, and Unknown.
+$ResultNumber = switch ($ResultText) {
+    Starting { 0 }
+    Started { 1 }
+    Stopping { 2 }
+    Stopped { 3 }
+    Unknown { 4 }
+    Default { 99 }
+}
+$ResultNumber
